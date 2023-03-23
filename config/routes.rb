58910d6a145ce3f6ccbc59ca229f595e2977789c
mairinjerse <mairinjerse@gmail.com>
2023-03-23 19:19:52 +0000

    refactoring - updating URls

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+  #get("/", { :controller => "movies", :action => "index" })
+  root "movies#index" 
+  
 
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
+  post "/movies" => "movies#create", as: :movies # gives us the methods: movies_url movies_path 
+  get "/movies/new" => "movies#new", as: :new_movie #gives us new_movie_url 
           
   # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
-  
+  get "/movies" => "movies#index" 
+  get "/movies/:id" => "movies#show", as: :movie #movie_path(argument) 
+
+
   # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  patch "/movies/:id" => "movies#update"
+  get "movies/:id/edit" => "movies#edit", as: :edit_movie #expects an argument edit_movie_path(argument) 
+
   
   # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
-
+  delete "/movies/:id" => "movies#destroy"
   #------------------------------
 end
